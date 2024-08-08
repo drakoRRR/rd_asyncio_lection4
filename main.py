e13c7fd165302bacb33701caff0925ec04c0d72a
Vlad <vladislav.musaelyan@renesandro.info>
2024-08-08 12:12:42 +0300

    fixed bugs, and added readme

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import asyncio
 import random
+from enum import Enum
+
+
+class ForkSides(Enum):
+    LEFT = "left"
+    RIGHT = "right"
 
 
 class Fork:
@@ -26,29 +32,39 @@ class Philosopher:
     async def eat(self):
         print(f"{self.name} wants to eat.")
         while True:
-            left_fork_picked = await self.try_pick_up_fork(self.left_fork, "left")
+            left_fork_picked = await self.try_pick_up_fork(self.left_fork, ForkSides.LEFT)
             if left_fork_picked:
-                right_fork_picked = await self.try_pick_up_fork(self.right_fork, "right")
+                right_fork_picked = await self.try_pick_up_fork(self.right_fork, ForkSides.RIGHT)
                 if right_fork_picked:
                     break
-                await self.put_down_fork(self.left_fork, "left")
-            await asyncio.sleep(random.uniform(0.1, 0.5))
+                await self.put_down_fork_one_closed(self.left_fork, ForkSides.LEFT)
+            return
 
         print(f"{self.name} starts eating.")
         await asyncio.sleep(random.uniform(1, 3))
 
-        await self.put_down_fork(self.right_fork, "right")
-        await self.put_down_fork(self.left_fork, "left")
+        await self.put_down_fork(self.right_fork, ForkSides.RIGHT)
+        await self.put_down_fork(self.left_fork, ForkSides.LEFT)
 
     async def try_pick_up_fork(self, fork, side):
-        if await fork.lock.acquire():
-            print(f"{self.name} picked up {side} fork.")
+        if not fork.lock.locked():
+            await fork.lock.acquire()
+            print(f"{self.name} picked up {side.value} fork.")
             return True
         return False
 
     async def put_down_fork(self, fork, side):
         fork.lock.release()
-        print(f"{self.name} put down {side} fork.")
+        print(f"{self.name} put down {side.value} fork.")
+
+    async def put_down_fork_one_closed(self, fork, side):
+        fork.lock.release()
+        if side == ForkSides.LEFT:
+            print(f"{self.name} put down {side.value} fork due to the reason the {ForkSides.RIGHT.value} fork"
+                  f" was taken by another philosopher.")
+        else:
+            print(f"{self.name} put down {side.value} fork due to the reason the {ForkSides.LEFT.value} fork"
+                  f" was taken by another philosopher.")
 
 
 async def main():
